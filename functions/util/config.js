afdb51e3964accd9aa7d9d575c85ab6baba9f0f8
Arthur <me@shihao.li>
2022-08-07 17:05:42 +0800

    test

--- functions/util/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  apiKey: "AIzaSyCLCQyP90qq6ncoS1XooorWXuBeuWIcJF0",
-  authDomain: "socialape-c8226.firebaseapp.com",
-  projectId: "socialape-c8226",
-  storageBucket: "socialape-c8226.appspot.com",
-  messagingSenderId: "877043317416",
-  appId: "1:877043317416:web:9a9360f0a4e45b531f609d",
-  measurementId: "G-YKKBJ6J88J",
-};
